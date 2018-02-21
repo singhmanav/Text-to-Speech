2dbeeaf1442618c7d11fc2f0a9540d7bd90409d0
Manav <manav@xebia.com>
2018-02-21 14:53:54 +0530

    Initial Project Setup

---
+++ Text-to-Speech/Text-to-Speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text-to-Speech
+//
+//  Created by manav on 21/02/18.
+//  Copyright © 2018 manav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
